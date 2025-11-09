7f270f66ec423ec2b185ae4130461212cfec1a0e
Joshua Lomond <joshualomond3503@gmail.com>
2025-11-09 15:22:54 -0400

    Add information for `Hartery's` project

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,17 +1,15 @@
 export const projects = [
   {
-    title: "Project One",
-    description:
-      "A brief description of this project. What it does, why I built it, and the tech-stack.",
+    title: "Hartery's",
+    description: "The official website of Hartery's Family Restaurant.",
     tags: ["Next.js", "Tailwind", "TypeScript"],
-    githubUrl: "https://github.com/joshualomond",
+    githubUrl: "https://github.com/joshualomond/harterys",
+    liveUrl: "https://harterys.com",
   },
   {
     title: "Project Two",
-    description:
-      "Another cool project. This one might focus on a different skill, like a complex backend API.",
+    description: "Another cool project.",
     tags: ["Node.js", "Express", "PostgreSQL"],
     githubUrl: "https://github.com/joshualomond",
-    liveUrl: "https://joshlomond.dev",
   },
 ];
